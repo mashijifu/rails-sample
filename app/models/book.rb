b42223a2e9b9af64eabf316832dd8768b18f0e17
mashijifu <4ts0t0i7c@gmail.com>
2026-02-14 18:14:02 +0900

    feat: Bookモデルにライフサイクルコールバックとログメソッドを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,24 @@
 class Book < ApplicationRecord
-    validates :title, presence: true
-    validates :description, presence: true, on: :update
+    before_validation :validation_prework
+    after_validation :validation_afterwork
+    before_save :save_prework, if: :description_present
+    after_save :save_afterwork, unless: :description_present
+    before_create :create_prework
+    after_create :create_afterwork
+    before_update :update_prework
+    after_update :update_afterwork
+    before_destroy :destroy_prework
+    after_destroy :destroy_afterwork
+    after_commit :commit_afterwork
+    after_rollback :rollback_afterwork
+  
+    validates :title, presence: true, length: {
+      in: 8..12,
+      too_long: "タイトルは最大12桁です",
+      too_short: "タイトルは最小8桁です"
+    }
     # validates :title, format: { with: /\A[a-zA-Z]+\z/, message: "は英文字で記述してください" }
-    validates :title, length: { in: 8..12,
-               too_long: "タイトルは最大12桁です",
-               too_short: "タイトルは最小8桁です",
-              }
+    validates :description, presence: true, on: :update
     validates :price, numericality: true
     validates :number, numericality: { only_integer: true}, uniqueness: true
     validates :genre, presence: true, if: :description_present
@@ -28,5 +41,55 @@ class Book < ApplicationRecord
         if title && title.match(/\A[a-zA-Z\s]+\z/) && !description.match(/\A[a-zA-Z\s]+\z/)
             errors.add(:description, "タイトルが英文字のときは、説明も英文字でなければならない")
         end
+    end
+
+    def validation_prework
+        puts "バリデーション前です"
+    end
+    
+    def validation_afterwork
+        puts "バリデーション後です"
+    end
+    
+    def save_prework
+        puts "保存前です"
+    end
+    
+    def save_afterwork
+        puts "保存後です"
+        # raise "Exception After_Save Error"
+    end
+    
+    def create_prework
+        puts "登録前です"
+    end
+    
+    def create_afterwork
+        puts "登録後です"
+    end
+    
+    def update_prework
+        puts "更新前です"
+    end
+    
+    def update_afterwork
+        puts "更新後です"
+    end
+    
+    def destroy_prework
+        puts "削除前です"
+    end
+    
+    def destroy_afterwork
+        puts "削除後です"
+        throw :abort
+    end
+    
+    def commit_afterwork
+        puts "コミット後です"
+    end
+    
+    def rollback_afterwork
+        puts "ロールバック後です"
     end
 end
